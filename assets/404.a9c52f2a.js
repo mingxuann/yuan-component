2cd045042e240acc34bbfd093b764adf7dd937c1
hemingxuan <15669147150@163.com>
2022-04-29 00:40:29 +0800

    deploy

--- assets/404.a9c52f2a.js
+++
@@ -1,1 +0,0 @@
-import{_,r as e}from"./index.2ebcfef6.js";const r={props:{msg:String},setup(t){return e(0),(s,a)=>"404"}};var c=_(r,[["__scopeId","data-v-052b70c5"]]);export{c as default};
---
+++ assets/index.c29d395f.js
@@ -0,0 +1,5 @@
+import{_ as s,r as c,o,c as n,a as r,t as i,F as p,d}from"./index.a31c1c80.js";const l=d(" ABOUT "),_={props:{msg:String},setup(a){c(0);const e=document.createElement("script");e.src="https://baike.baidu.com/api/openapi/BaikeLemmaCardApi?appid=379020&bk_key=\u77F3\u69B4&callback=getResultList",document.body.appendChild(e);const t=document.createElement("script");return t.innerHTML=`
+    function getResultList(res) {
+        console.log("res", res)
+    }
+`,document.body.appendChild(t),(u,m)=>(o(),n(p,null,[l,r("h1",null,i(a.msg),1)],64))}};var f=s(_,[["__scopeId","data-v-7621ffcb"]]);export{f as default};
