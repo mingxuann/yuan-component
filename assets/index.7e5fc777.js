8e7be32072d889380c85edf9a27c96f83e5abc65
hemingxuan <15669147150@163.com>
2022-04-29 22:20:00 +0800

    deploy

---
+++ assets/index.7e5fc777.js
@@ -0,0 +1,5 @@
+import{_ as o,f as a,o as n,c as r,a as c,t as p,F as d,b as i,p as _,e as l}from"./index.8364ae73.js";const m=e=>(_("data-v-2ec853ea"),e=e(),l(),e),u=i(" ABOUT "),h=m(()=>c("img",{src:"https://hemingxaun-1256953833.cos.ap-shanghai.myqcloud.com/img1.jpeg",alt:""},null,-1)),g={props:{msg:String},setup(e){a(0);const t=document.createElement("script");t.src="https://j.i8tq.com/weather2020/search/city.js",document.body.appendChild(t);const s=document.createElement("script");return s.innerHTML=`
+    function getResultList(res) {
+        console.log("res", res)
+    }
+`,document.body.appendChild(s),(x,f)=>(n(),r(d,null,[u,h,c("h1",null,p(e.msg),1)],64))}};var S=o(g,[["__scopeId","data-v-2ec853ea"]]);export{S as default};
