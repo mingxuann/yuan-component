375273fc4fcc3341ba9b8b92f95413e65f36a383
hemingxuan <15669147150@163.com>
2022-04-29 20:24:42 +0800

    deploy

--- assets/index.d95a1177.js
+++
@@ -1,5 +0,0 @@
-import{_ as c,f as o,o as r,c as a,a as n,t as d,F as i,b as p}from"./index.f0a1be4d.js";const _=p(" ABOUT "),l={props:{msg:String},setup(s){o(0);const e=document.createElement("script");e.src="https://j.i8tq.com/weather2020/search/city.js",document.body.appendChild(e);const t=document.createElement("script");return t.innerHTML=`
-    function getResultList(res) {
-        console.log("res", res)
-    }
-`,document.body.appendChild(t),(u,m)=>(r(),a(i,null,[_,n("h1",null,d(s.msg),1)],64))}};var h=c(l,[["__scopeId","data-v-54b7a4b6"]]);export{h as default};
